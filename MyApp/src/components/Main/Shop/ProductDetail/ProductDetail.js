6f6ac227ec675b5532fd1ad2773a54b6ea77278a
vanminhnguyenbmt <vanminhnguyenbmt@gmail.com>
2017-12-28 11:38:11 +0700

    Finished Layout Screen ProductDetail

--- MyApp/src/components/Main/Shop/ProductDetail/ProductDetail.js
+++ MyApp/src/components/Main/Shop/ProductDetail/ProductDetail.js
@@ -5,13 +5,14 @@ import { View, Text, StyleSheet, TouchableOpacity, Dimensions,
 import iconBack from '../../../../../src/img/appIcon/back.png';
 import iconCart from '../../../../../src/img/appIcon/cartfull.png';
 import sp3 from '../../../../../src/img/temp/sp3.jpeg';
-import sp5 from '../../../../../src/img/temp/sp5.jpeg';
+import sp4 from '../../../../../src/img/temp/sp4.jpeg';
 
 export default class ProductDetail extends Component {
     render() {
         const { wrapper, container, header, wrapImg, wrapContent,
                 iconStyle, imgStyle, wrapImgElement, titleContent,
-                txtTitle, txtDevide, txtPrice, txtContent, footer } = styles;
+                txtTitle, txtDevide, txtPrice, txtContent, footer,
+                txtContentStyle, txtFooter, txtColor, wrapFooterColor, colorStyle } = styles;
         return (
             <View style={wrapper}>
                 <View style={container}>
@@ -29,7 +30,7 @@ export default class ProductDetail extends Component {
                                 <Image source={sp3} style={imgStyle} />
                             </View>
                             <View style={wrapImgElement}>
-                                <Image source={sp5} style={imgStyle} />
+                                <Image source={sp4} style={imgStyle} />
                             </View>
                         </ScrollView>
                     </View>
@@ -40,10 +41,14 @@ export default class ProductDetail extends Component {
                             <Text style={txtPrice}>200$</Text>
                         </View>
                         <View style={txtContent}>
-
+                            <Text style={txtContentStyle}>A delicate layer of eyelash lace brings dreamy elegance to this piece, while smooth, lightweight lining feels luxurious against your skin. We love it with heels for a look that fits in on date night, or with cool booties to add an edge.</Text>
                         </View>
                         <View style={footer}>
-
+                            <Text style={txtFooter}>Material Leather</Text>
+                            <View style={wrapFooterColor}>
+                                <Text style={txtColor}>Color Orange</Text>
+                                <View style={colorStyle} />
+                            </View>
                         </View>
                     </View>
                 </View>
@@ -53,6 +58,8 @@ export default class ProductDetail extends Component {
 }
 
 const { width, height } = Dimensions.get('window');
+const imgWidth = width * 0.9;
+const imgHeight = (imgWidth * 452) / 361;
 const styles = StyleSheet.create({
     wrapper: {
         backgroundColor: '#d8dde0',
@@ -71,28 +78,32 @@ const styles = StyleSheet.create({
         paddingHorizontal: 5,
     },
     wrapImg: {
-        flex: 1,
-        borderWidth: 1
+        flex: 1
     },
     wrapContent: {
-        flex: 1
+        flex: 1,
+        paddingHorizontal: 10
     },
     iconStyle: {
         width: 33,
         height: 33
     },
     wrapImgElement: {
-        marginLeft: 10
+        marginLeft: 10,
+        width: imgWidth,
+        height: imgHeight
     },
     imgStyle: {
-        width: width * 0.9,
-        height: ((width * 0.9) * 452) / 361,
+        width: imgWidth,
+        height: imgHeight
     },
     titleContent: {
-        flex: 1,
+        flex: 2,
         flexDirection: 'row',
         justifyContent: 'center',
-        borderWidth: 1
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderColor: '#E2E4E9',
     },
     txtTitle: {
         fontSize: 22,
@@ -103,14 +114,42 @@ const styles = StyleSheet.create({
         fontSize: 22
     },
     txtPrice: {
-        fontSize: 22
+        fontSize: 22,
+        fontFamily: 'monospace'
     },
     txtContent: {
         flex: 4,
-        borderWidth: 1
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        paddingVertical: 10
+    },
+    txtContentStyle: {
+        fontFamily: 'sans-serif-medium',
+        fontSize: 15,
+        color: '#888888'
     },
     footer: {
         flex: 1,
-        borderWidth: 1
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    txtFooter: {
+        color: '#C52F79',
+        fontSize: 17
+    },
+    wrapFooterColor: {
+        flexDirection: 'row'
+    },
+    txtColor: {
+        color: '#C52F79',
+        fontSize: 17,
+        marginRight: 20
+    },
+    colorStyle: {
+        width: 20,
+        height: 20,
+        borderRadius: 20 / 2,
+        backgroundColor: 'ORANGE'.toLowerCase(),
+        borderColor: '#C52F79'
     }
 });
